4fb2e60341dc63ddae9bee670aebc5593f2785ab
Shamir Maharjan <shamirmaharjan07@gmail.com>
2025-07-02 12:12:20 +0545

    Configure MongoDB setup

--- backend/src/config/env.js
+++ backend/src/config/env.js
@@ -6,5 +6,14 @@ import { config } from "dotenv";
 config({ path: `.env.${process.env.NODE_ENV || 'development'}.local` });
 
 export const {
-    PORT
+    PORT,
+    NODE_ENV,
+    MONGO_URI,
+    CLERK_PUBLISHABLE_KEY,
+    CLERK_SECRET_KEY,
+    ARCJET_KEY,
+    ARCJET_ENV,
+    CLOUDINARY_CLOUD_NAME,
+    CLOUDINARY_API_KEY,
+    CLOUDINARY_API_SECRET
 } = process.env;
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,13 @@
 import express from 'express';
-
+import { PORT } from './config/env.js';
+import connectToDatabase from './database/mongodb.js';
 const app = express();
 
 app.get("/", (req, res) => {
     res.send("Hello World");
 });
 
-app.listen(3000, () => {
-    console.log("Server is running on port 3000");
+app.listen(PORT, async () => {
+    console.log(`Server is running on port ${PORT}`);
+    await connectToDatabase();
 })
